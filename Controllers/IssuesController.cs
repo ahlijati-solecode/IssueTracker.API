9ad90970d3ce9d5309afa2a60cdc32507e1ff915
dzakitajudin <dzakitajudin@gmail.com>
2025-09-25 12:13:19 +0700

    update issue controller

--- Controllers/IssuesController.cs
+++ Controllers/IssuesController.cs
@@ -1,35 +1,96 @@
-﻿using IssueTracker.API.Data;
-using IssueTracker.API.Interfaces;
-using IssueTracker.API.Services;
-using Microsoft.EntityFrameworkCore;
+﻿using IssueTracker.API.Interfaces;
+using IssueTracker.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
-var builder = WebApplication.CreateBuilder(args);
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var connectionString =
-builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddScoped<IIssueService, IssueService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace IssueTracker.API.Controllers
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssuesController : ControllerBase
+    {
+        private readonly IIssueService _issueService;
+
+        public IssuesController(IIssueService issueService)
+        {
+            _issueService = issueService;
+        }
+
+        //private static List<Issue> _issues = new List<Issue>
+        //{
+        //    new Issue { id = 1, Title = "Menu Bugs", Description = "menu bugs description", Status = "Active", Priority = "Major", ProjectId = 1, AssignedToUserId = "Dzaki", CreatedDate = DateTime.Now },
+        //    new Issue { id = 2, Title = "Upload Bugs", Description = "upload bugs description", Status = "Active", Priority = "Minor", ProjectId = 1, AssignedToUserId = "Lisdiana", CreatedDate = DateTime.Now },
+        //};
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllIssues()
+        {
+            var _issues = await _issueService.GetIssuesAsync();
+            return Ok(_issues);
+        }
+
+        // GET api/<IssueController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetIssueById(int id)
+        {
+            var issue = await _issueService.GetIssueByIdAsync(id);
+            if (issue == null)
+            {
+                return NotFound($"Issue with ID = {id} not found.");
+            }
+
+            return Ok(issue);
+        }
+
+        // POST api/<IssueController>
+        [HttpPost]
+        public async Task<IActionResult> CreateIssue([FromBody] Issue newIssue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newsissue = await _issueService.CreateAsync(newIssue);
+
+            return CreatedAtAction(nameof(GetIssueById),
+                new { id = newIssue.id }, newIssue);
+        }
+
+        // PUT api/<IssueController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateIssue(int id, [FromBody] Issue updateIssue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Update data
+            var updated = await _issueService.UpdateAsync(id, updateIssue);
+            if (updated == null)
+            {
+                return NotFound($"Issue with ID = {id} not found.");
+            }
+
+            return Ok(updated);
+
+        }
+
+        // DELETE api/<IssueController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIssueAsync(int id)
+        {
+            // Delete data
+            var success = await _issueService.DeleteAsync(id);
+            if (!success)
+            {
+                return NotFound($"Issue with ID = {id} not found.");
+            }
+
+            return Ok($"Issue with ID = {id} has been deleted");
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
